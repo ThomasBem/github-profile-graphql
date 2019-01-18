485a1f0932e717944c402ad6b115a985f465063c
Thomas Bembridge <thomas.bembridge@fmcti.com>
2019-01-18 22:02:15 +0100

    Set height on github component so that it doesnt resize based on lack of bio etc

--- src/github/profile/avatar/Avatar.js
+++ src/github/profile/avatar/Avatar.js
@@ -6,6 +6,6 @@ const Avatar = ({ avatarUrl, githubUrl, openGithubProfile }) => (
   <div className={style.root}>
     <img src={avatarUrl} alt="Avatar" onClick={() => openGithubProfile(githubUrl)} />
   </div>
-)
+);
 
 export default Avatar;
